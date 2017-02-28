f24a2339696c2b6bcddebfbb90e7d1463bfbc1f0
max <51796875@qq.com>
2017-02-28 17:27:01 +0800

    创建权限相关表

---
+++ database/migrations/2017_02_28_084536_create_user_role_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->comment('用户ID对应users表')->index();
+            $table->unsignedInteger('role_id')->comment('角色id对应roles表');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_role');
+    }
+}
---
+++ database/migrations/2017_02_28_090206_create_role_menu_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_menu', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('role_id')->comment('角色id对应roles表')->index();
+            $table->unsignedInteger('menu_id')->comment('菜单ID对应menus表');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_menu');
+    }
+}
